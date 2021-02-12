6f1283c28cbbfb13838ddc6120788faca5aa8c2e
shanole <73411129+shanole@users.noreply.github.com>
2021-02-11 20:44:44 -0800

    Pass test 10

--- js/scripts.js
+++ js/scripts.js
@@ -2,39 +2,44 @@
 
 function mrRoboger(num) {
   let output = []
-  for (let i=0; i <= num; i++) {
-    const numString = i.toString();
-    if (numString.length === 1) {
-      for (const digit of numString) {
-        if (digit === "1") {
-          output.push("Beep");
-          break;
+  if (typeof num === "number") {
+    for (let i=0; i <= num; i++) {
+      const numString = i.toString();
+      if (numString.length === 1) {
+        for (const digit of numString) {
+          if (digit === "1") {
+            output.push("Beep");
+            break;
+          }
+          else if (digit === "2") {
+            output.push("Boop");
+            break;
+          }
+          else if (digit === "3") {
+            output.push("Won't you be my neighbor?");
+          }
+          else {
+            output.push(i);
+          }
         }
-        else if (digit === "2") {
-          output.push("Boop");
-          break;
-        }
-        else if (digit === "3") {
-          output.push("Won't you be my neighbor?");
-        }
-        else {
-          output.push(i);
-        }
-      }
-    }
-    else {
-      if (numString.includes("1") === true && numString.includes("2") === false && numString.includes("3") === false) {
-        output.push("Beep")
-      }
-      else if (numString.includes("2") === true && numString.includes("3") === false) {
-        output.push("Boop")
       }
       else {
-        output.push("Won't you be my neighbor?")
+        if (numString.includes("1") === true && numString.includes("2") === false && numString.includes("3") === false) {
+          output.push("Beep")
+        }
+        else if (numString.includes("2") === true && numString.includes("3") === false) {
+          output.push("Boop")
+        }
+        else {
+          output.push("Won't you be my neighbor?")
+        }
       }
     }
+    return output;
   }
-  return output;
+  else {
+    return "Sorry, please give me a number.";
+  }
 }
 
 // UI logic
